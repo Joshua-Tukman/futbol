2da1bbeadf903cfb7adff1295a7b8ee7d7b46de1
Andrew Reid <areid.sj@gmail.com>
2020-02-18 13:15:14 -0700

    Refactor game teams test to treat incoming data as strings

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -14,15 +14,15 @@ class GameTeamsTest < Minitest::Test
       result: "LOSS",
       settled_in: "OT",
       head_coach: "John Tortorella",
-      goals: 2,
-      shots: 8,
-      tackles: 44,
-      pim: 8,
-      powerPlayOpportunities: 3,
-      powerPlayGoals: 0,
-      faceOffWinPercentage: 44.8,
-      giveaways: 17,
-      takeaways: 7
+      goals: '2',
+      shots: '8',
+      tackles: '44',
+      pim: '8',
+      powerPlayOpportunities: '3',
+      powerPlayGoals: '0',
+      faceOffWinPercentage: '44.8',
+      giveaways: '17',
+      takeaways: '7'
     }
 
     @game_team = GameTeams.new(params)
@@ -39,14 +39,14 @@ class GameTeamsTest < Minitest::Test
     assert_equal "LOSS", @game_team.result
     assert_equal "OT", @game_team.settled_in
     assert_equal "John Tortorella", @game_team.head_coach
-    assert_equal 2, @game_team.goals
-    assert_equal 8, @game_team.shots
-    assert_equal 44, @game_team.tackles
-    assert_equal 8, @game_team.pim
-    assert_equal 3, @game_team.powerPlayOpportunities
-    assert_equal 0, @game_team.powerPlayGoals
-    assert_equal 44.8, @game_team.faceOffWinPercentage
-    assert_equal 17, @game_team.giveaways
-    assert_equal 7, @game_team.takeaways
+    assert_equal '2', @game_team.goals
+    assert_equal '8', @game_team.shots
+    assert_equal '44', @game_team.tackles
+    assert_equal '8', @game_team.pim
+    assert_equal '3', @game_team.powerPlayOpportunities
+    assert_equal '0', @game_team.powerPlayGoals
+    assert_equal '44.8', @game_team.faceOffWinPercentage
+    assert_equal '17', @game_team.giveaways
+    assert_equal '7', @game_team.takeaways
   end
 end
