530095d8461d61c4b48aba076048a232fbef5715
Andrew Reid <areid.sj@gmail.com>
2020-02-18 08:32:21 -0700

    Pass Team has attibutes test

--- test/teams_test.rb
+++ test/teams_test.rb
@@ -14,7 +14,7 @@ class TeamTest < Minitest::Test
     franchiseId: "23",
     teamName: "Atlanta United",
     abbreviation: "ATL",
-    Stadium: "Mercedes-Benz Stadium",
+    stadium: "Mercedes-Benz Stadium",
     link: "/api/v1/teams/1"
   }
 
@@ -23,6 +23,15 @@ class TeamTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Team, @team
+  end
+
+  def test_it_has_attributes
+    assert_equal "1", @team.team_id
+    assert_equal "23", @team.franchiseId
+    assert_equal "Atlanta United", @team.teamName
+    assert_equal "ATL", @team.abbreviation
+    assert_equal "Mercedes-Benz Stadium", @team.stadium
+    assert_equal "/api/v1/teams/1", @team.link
   end
 
 
