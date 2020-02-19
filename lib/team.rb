f02083a1a2c2cb12b3aca6607c44bb3994f7feec
Tyler Porter <tylerpporter@gmail.com>
2020-02-18 19:34:49 -0700

    Refactor Team class to create and hold all instances of Team from csv file

--- lib/team.rb
+++ lib/team.rb
@@ -1,4 +1,20 @@
+require 'csv'
+
 class Team
+  @@all_teams = nil
+
+  def self.load_csv(file_path)
+    @@all_teams = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      teams_params = row.to_hash
+      @@all_teams << Team.new(teams_params)
+    end
+  end
+
+  def self.all
+    @@all_teams
+  end
+
   attr_reader :team_id,
               :franchiseid,
               :teamname,
--- test/team_test.rb
+++ test/team_test.rb
@@ -13,6 +13,8 @@ class TeamTest < Minitest::Test
       link: "/api/v1/teams/1"
     }
     @team = Team.new(params)
+    Team.load_csv('./data/sample_teams.csv')
+    @new_team = Team.all[1]
   end
 
   def test_it_exists
@@ -26,6 +28,21 @@ class TeamTest < Minitest::Test
     assert_equal "ATL", @team.abbreviation
     assert_equal "Mercedes-Benz Stadium", @team.stadium
     assert_equal "/api/v1/teams/1", @team.link
+  end
+
+  def test_it_can_create_teams_from_csv
+    assert_instance_of Team, @new_team
+    assert_equal "2", @new_team.team_id
+    assert_equal "22", @new_team.franchiseid
+    assert_equal "Seattle Sounders FC", @new_team.teamname
+    assert_equal "SEA", @new_team.abbreviation
+    assert_equal "Centruy Link Field", @new_team.stadium
+    assert_equal "/api/v1/teams/2", @new_team.link
+  end
+
+  def test_it_contains_all_teams
+    assert_instance_of Team, Team.all.sample
+    assert_equal 2, Team.all.size 
   end
 
 end
