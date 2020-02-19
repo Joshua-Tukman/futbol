dcd4e97a926e68798b02964a03a528a1fe6551ca
Tyler Porter <tylerpporter@gmail.com>
2020-02-18 20:53:20 -0700

    Refactor GameTeams class to create and hold all instances of GameTeams from csv file

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -22,6 +22,8 @@ class GameTeamsTest < Minitest::Test
       takeaways: '7'
     }
     @game_team = GameTeams.new(params)
+    GameTeams.load_csv('./data/game_teams_sample.csv')
+    @new_data = GameTeams.all[0]
   end
 
   def test_it_exists
@@ -29,21 +31,45 @@ class GameTeamsTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "2012030221", @game_team.game_id
-    assert_equal "3", @game_team.team_id
+    assert_equal 2012030221, @game_team.game_id
+    assert_equal 3, @game_team.team_id
     assert_equal "away", @game_team.hoa
     assert_equal "LOSS", @game_team.result
     assert_equal "OT", @game_team.settled_in
     assert_equal "John Tortorella", @game_team.head_coach
-    assert_equal '2', @game_team.goals
-    assert_equal '8', @game_team.shots
-    assert_equal '44', @game_team.tackles
-    assert_equal '8', @game_team.pim
-    assert_equal '3', @game_team.powerplayopportunities
-    assert_equal '0', @game_team.powerplaygoals
-    assert_equal '44.8', @game_team.faceoffwinpercentage
-    assert_equal '17', @game_team.giveaways
-    assert_equal '7', @game_team.takeaways
+    assert_equal 2, @game_team.goals
+    assert_equal 8, @game_team.shots
+    assert_equal 44, @game_team.tackles
+    assert_equal 8, @game_team.pim
+    assert_equal 3, @game_team.powerplayopportunities
+    assert_equal 0, @game_team.powerplaygoals
+    assert_equal 44.8, @game_team.faceoffwinpercentage
+    assert_equal 17, @game_team.giveaways
+    assert_equal 7, @game_team.takeaways
+  end
+
+  def test_it_can_create_game_team_data_objects_from_csv
+    assert_instance_of GameTeams, @new_data
+    assert_equal 2012030111, @new_data.game_id
+    assert_equal 2, @new_data.team_id
+    assert_equal "away", @new_data.hoa
+    assert_equal "LOSS", @new_data.result
+    assert_equal "REG", @new_data.settled_in
+    assert_equal "Jack Capuano", @new_data.head_coach
+    assert_equal 0, @new_data.goals
+    assert_equal 6, @new_data.shots
+    assert_equal 41, @new_data.tackles
+    assert_equal 51, @new_data.pim
+    assert_equal 4, @new_data.powerplayopportunities
+    assert_equal 0, @new_data.powerplaygoals
+    assert_equal 51.6, @new_data.faceoffwinpercentage
+    assert_equal 4, @new_data.giveaways
+    assert_equal 3, @new_data.takeaways
+  end
+
+  def test_it_contains_all_game_team_data_objects
+    assert_instance_of GameTeams, GameTeams.all.sample
+    assert_equal 945, GameTeams.all.size
   end
 
 end
